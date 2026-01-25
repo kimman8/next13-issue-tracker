bdb3788290fc833ea2bc21d3b4f100a8277adcf8
KIM YUEN <kim_man@live.com>
2026-01-25 14:41:28 +1100

    style active link in navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,10 @@
+'use client';
+
 import Link from 'next/link';
 import React from 'react';
 import { FaBug } from 'react-icons/fa';
+import { usePathname } from 'next/navigation';
+import classnames from 'classnames';
 
 const NavBar = () => {
   const links = [
@@ -13,8 +17,10 @@ const NavBar = () => {
       label: 'Issues',
     },
   ];
+  const currentPath = usePathname();
+  console.log(currentPath);
   return (
-    <nav className="flex border-b mb-5 px-3 h-14 items-center bg-">
+    <nav className="flex border-b mb-5 px-3 h-14 items-center">
       <Link className="mr-3" href="/">
         <FaBug />
       </Link>
@@ -23,7 +29,11 @@ const NavBar = () => {
           <Link
             key={link.label}
             href={link.href}
-            className="text-zinc-500 hover:text-zinc-900 transition-colors"
+            className={classnames({
+              'text-zinc-900': currentPath === link.href,
+              'text-zinc-400': currentPath !== link.href,
+              'hover:text-zinc-800 transition-colors': true,
+            })}
           >
             {link.label}
           </Link>
