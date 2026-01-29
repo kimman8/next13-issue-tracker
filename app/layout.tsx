e9541bb89530a416bbe91f47be1e1c305f9750de
KIM YUEN <kim_man@live.com>
2026-01-29 18:18:40 +1100

    build the new issue page

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <NavBar />
         <Theme>
-          <main>{children}</main>
+          <main className="p-5">{children}</main>
         </Theme>
       </body>
     </html>
