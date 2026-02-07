1c52e03e9543ae6a58e1f45db11691d983f5a0d4
KIM YUEN <kim_man@live.com>
2026-02-07 14:31:50 +1100

    show issue details

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,23 @@
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+
+const LoadingIssueDetailPage = () => {
+  return (
+    <div>
+      <p>
+        <Skeleton />
+      </p>
+      <p>
+        <Skeleton />
+      </p>
+      <p>
+        <Skeleton />
+      </p>
+      <p>
+        <Skeleton />
+      </p>
+    </div>
+  );
+};
+
+export default LoadingIssueDetailPage;
---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,29 @@
+import { prisma } from '@/prisma/client';
+import delay from 'delay';
+import { notFound } from 'next/navigation';
+
+interface Props {
+  params: { id: string };
+}
+const IssueDetailPage = async ({ params }: Props) => {
+  if (typeof params.id !== 'number') notFound();
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+  if (!issue) notFound();
+  console.log(issue);
+  await delay(2000);
+
+  return (
+    <div>
+      <p>{issue.title}</p>
+      <p>{issue.description}</p>
+      <p>{issue.status}</p>
+      <p>{issue.createdAt.toDateString()}</p>
+    </div>
+  );
+};
+
+export default IssueDetailPage;
