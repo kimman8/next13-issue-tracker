e8ca29af2e9ee379d77304399633ca2537cd4cbb
KIM YUEN <kim_man@live.com>
2026-02-08 13:00:55 +1100

    add additional loading skeletons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,22 +1,19 @@
+import { Heading, Flex, Card, Text, Box } from '@radix-ui/themes';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 
 const LoadingIssueDetailPage = () => {
   return (
-    <div>
-      <p>
-        <Skeleton />
-      </p>
-      <p>
-        <Skeleton />
-      </p>
-      <p>
-        <Skeleton />
-      </p>
-      <p>
-        <Skeleton />
-      </p>
-    </div>
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Flex className="space-x-3" my="2">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card className="prose" mt="4">
+        <Skeleton count={3} />
+      </Card>
+    </Box>
   );
 };
 
