4eb0ee09d436cf732a0572c7f704ef2842b8c5fe
KIM YUEN <kim_man@live.com>
2026-02-10 18:09:15 +1100

    reafctor: apply the SRP

---
+++ app/issues/[id]/EditIssueButton.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@radix-ui/themes';
+import Link from 'next/link';
+import { Pencil2Icon } from '@radix-ui/react-icons';
+
+interface Props {
+  issueId: number;
+}
+const EditIssueButton = ({ issueId }: Props) => {
+  return (
+    <Button>
+      <Pencil2Icon />
+      <Link href={`/issues/${issueId}/edit`}>Edit Issue</Link>
+    </Button>
+  );
+};
+
+export default EditIssueButton;
