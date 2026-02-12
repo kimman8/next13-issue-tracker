30e6e18c97ff465a24df904d184f244c26a4de7d
KIM YUEN <kim_man@live.com>
2026-02-12 17:55:06 +1100

    improve the loading experience

--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,6 +1,12 @@
-import IssueForm from '../../_components/IssueForm';
 import { prisma } from '@/prisma/client';
 import { notFound } from 'next/navigation';
+import dynamic from 'next/dynamic';
+import IssueFormSkeleton from '../../_components/IssueFormSkeleton';
+
+const IssueForm = dynamic(() => import('../../_components/IssueForm'), {
+  ssr: false,
+  loading: () => <IssueFormSkeleton />,
+});
 
 interface Props {
   params: { id: string };
