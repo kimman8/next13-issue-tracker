6aceee259a929822b8b9df5253c4657e362531de
KIM YUEN <kim_man@live.com>
2026-02-12 13:47:07 +1100

    update issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -38,7 +38,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
   const onSubmit = handleSubmit(async (data: IssueFormData) => {
     try {
       setIsSubmitting(true);
-      await axios.post('/api/issues', data);
+      if (issue) await axios.patch('/api/issues/' + issue.id, data);
+      else await axios.post('/api/issues', data);
       router.push('/issues');
     } catch (error) {
       setIsSubmitting(false);
@@ -71,7 +72,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button disabled={isSubmitting}>
-          {issue ? 'Edit Issue' : 'Submit new issue'}
+          {issue ? 'Update Issue' : 'Submit new issue'}
           {isSubmitting && <Spinner />}
         </Button>
       </form>
